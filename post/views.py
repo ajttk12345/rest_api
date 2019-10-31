f5469977473f0fac69339b7691503b87abc903fe
studyregchoikyungjin <ickz5563@likelion.org>
2019-10-31 11:49:13 +0900

    init

---
+++ post/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from .models import Post
+from .serializer import PostSerializer
+from rest_framework import viewsets
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+# Create your views here.
